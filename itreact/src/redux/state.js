1826ef7e8d844381de6c7f81f72771d1ddeb00a2
MykhailovMisha <mihalov.misha@gmail.com>
2020-03-20 17:11:35 +0200

    12

--- itreact/src/redux/state.js
+++ itreact/src/redux/state.js
@@ -1,72 +1,75 @@
- let store = {
-    _state: {
-      profilePage: {
-        postData: [ 
-        { id: 1, alert: "Hi,howau", like:12 },
-        { id: 2, alert: "ItsMyFirstPrjct", like:76 },
-        { id: 3, alert: "Look at me now", like:726 },
-        { id: 4, alert: 'message', like: 23},
-        { id: 5, alert: 'lol', like: 43}
+let store = {
+  _state: {
+    profilePage: {
+      postData: [
+        { id: 1, alert: "Hi,howau", like: 12 },
+        { id: 2, alert: "ItsMyFirstPrjct", like: 76 },
+        { id: 3, alert: "Look at me now", like: 726 },
+        { id: 4, alert: "message", like: 23 },
+        { id: 5, alert: "lol", like: 43 }
       ],
-        newPostText: ''
-      },
-  
-      musicPage: {
-        MusicData: [
-        {id: 1, name: "props"},
-        {id: 2, name: "react"},
-        {id: 3, name: 'redux'},
-        {id: 4, name:  "Ramil'"},
-        {id: 5, name: '104'}
-      ],
-      },
-      
+      newPostText: ""
+    },
+
+    musicPage: {
+      MusicData: [
+        { id: 1, name: "props" },
+        { id: 2, name: "react" },
+        { id: 3, name: "redux" },
+        { id: 4, name: "Ramil'" },
+        { id: 5, name: "104" }
+      ]
+    },
+
     dialogsPage: {
-      dialogsData: [   
-        { id: 1, name: "Andrew", avatar: "https://en.facebookbrand.com/wp-content/uploads/2019/06/f_logo_RGB-Blue_512.png" },
+      dialogsData: [
+        {
+          id: 1,
+          name: "Andrew",
+          avatar:
+            "https://en.facebookbrand.com/wp-content/uploads/2019/06/f_logo_RGB-Blue_512.png"
+        },
         { id: 2, name: "Sasha" },
         { id: 3, name: "Dymich" },
         { id: 4, name: "Sveta" },
-        { id: 5, name: "Valera" },
+        { id: 5, name: "Valera" }
       ],
-       messagesData: [ 
+      messagesData: [
         { id: 1, message: "Mwssage one" },
         { id: 2, message: "Message two" },
         { id: 3, message: "Message three" },
         { id: 4, message: "Message four" },
         { id: 5, message: "Message five" }
       ]
-      }
-    },
-    _rerenderEntireTree() {
-      console.log("state");
-   }, 
+    }
+  },
+  _rerenderEntireTree() {
+    console.log("state");
+  },
 
-    getState() {
-      return this._state;
-    },
-    subscribe(observer) {
-      this._rerenderEntireTree = observer;
-    },
+  getState() {
+    return this._state;
+  },
+  subscribe(observer) {
+    this._rerenderEntireTree = observer;
+  },
 
-   
-    dispatch(action){
-      if (action.type === 'ADD-POST'){
-        let newPost = {
-          id: 5,
-          alert: this._state.profilePage.newPostText,
-          like: 0
+  dispatch(action) {
+    if (action.type === "ADD-POST") {
+      let newPost = {
+        id: 5,
+        alert: this._state.profilePage.newPostText,
+        like: 0
       };
       this._state.profilePage.postData.push(newPost);
-      this._state.profilePage.newPostText = '' ;
+      this._state.profilePage.newPostText = "";
       this._rerenderEntireTree(this._state);
-      } 
-      else if (action.type === 'UPDATE-NEW-TEXT'){
-        this._state.profilePage.newPostText = action.NewText; 
-        this._rerenderEntireTree(this._state);
-      }
+    } else if (action.type === "UPDATE-NEW-TEXT") {
+      this._state.profilePage.newPostText = action.NewText;
+      this._rerenderEntireTree(this._state);
     }
- }
+  }
+};
 
-  window.store = store;   
-  export default store;
+window.store = store;
+export default store;
